f748aae9ba0933c6f72128e62b00a470f5439702
santa112358 <sun.jp.0212@gmail.com>
2020-05-31 22:32:37 +0900

    add pubspec in example

---
+++ example/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
