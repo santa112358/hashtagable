5631deab2a6a77baa572cdb8fdbd7bc318a2bd10
santa112358 <sun.jp.0212@gmail.com>
2020-06-30 16:28:59 +0900

    fix example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,34 +13,67 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          RichText(
-            text: getHashTagTextSpan(
-              TextStyle(fontSize: 14, color: Colors.red),
-              TextStyle(fontSize: 14, color: Colors.black),
-              "#Hello world. Hello #world",
-              (text) {
-                print(text);
-              },
-            ),
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              RichText(
+                text: getHashTagTextSpan(
+                  TextStyle(fontSize: 14, color: Colors.red),
+                  TextStyle(fontSize: 14, color: Colors.black),
+                  "#Hello world. Hello #world",
+                  (text) {
+                    print(text);
+                  },
+                ),
+              ),
+              Text("Without hint text "),
+              const SizedBox(
+                height: 8,
+              ),
+
+              /// You don't need to add decorated box. This is to make it visible
+              DecoratedBox(
+                decoration: BoxDecoration(border: Border.all()),
+                child: HashTagEditableText(
+                  decoratedStyle: TextStyle(fontSize: 14, color: Colors.black),
+                  basicStyle: TextStyle(fontSize: 14, color: Colors.blue),
+                  cursorColor: Theme.of(context).cursorColor,
+                  controller: TextEditingController(),
+                  maxLines: 4,
+                  keyboardType: TextInputType.multiline,
+                ),
+              ),
+              TextField(
+                maxLength: 4,
+                maxLengthEnforced: false,
+              ),
+              const SizedBox(
+                height: 48,
+              ),
+              Text("With hint text "),
+              const SizedBox(
+                height: 8,
+              ),
+
+              /// You don't need to add decorated box. This is to make it visible
+              DecoratedBox(
+                decoration: BoxDecoration(border: Border.all()),
+                child: HashTagEditableTextWithHintText(
+                  hintText: "THIS IS HINT TEXT",
+                  hintTextStyle: TextStyle(
+                      fontSize: 14, color: Theme.of(context).hintColor),
+                  basicStyle: TextStyle(fontSize: 14, color: Colors.black),
+                  decoratedStyle: TextStyle(fontSize: 14, color: Colors.blue),
+                  cursorColor: Theme.of(context).cursorColor,
+                ),
+              ),
+            ],
           ),
-          HashTagEditableText(
-            decoratedStyle: TextStyle(fontSize: 14, color: Colors.black),
-            basicStyle: TextStyle(fontSize: 14, color: Colors.blue),
-            cursorColor: Theme.of(context).cursorColor,
-            controller: TextEditingController(),
-          ),
-          HashTagEditableTextWithHintText(
-            hintText: "THIS IS HINT TEXT",
-            hintTextStyle:
-                TextStyle(fontSize: 14, color: Theme.of(context).hintColor),
-            basicStyle: TextStyle(fontSize: 14, color: Colors.black),
-            decoratedStyle: TextStyle(fontSize: 14, color: Colors.blue),
-            cursorColor: Theme.of(context).cursorColor,
-          ),
-        ],
+        ),
       ),
     );
   }
