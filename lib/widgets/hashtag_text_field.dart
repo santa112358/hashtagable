dd41ea7d6539e55730f9ae1663ae590947207e9c
Mesut UZUN <mesutuzun@gmail.com>
2021-05-15 14:27:54 +0300

    null safe fix

--- lib/widgets/hashtag_text_field.dart
+++ lib/widgets/hashtag_text_field.dart
@@ -42,7 +42,7 @@ class _TextFieldSelectionGestureDetectorBuilder
     extends TextSelectionGestureDetectorBuilder {
   _TextFieldSelectionGestureDetectorBuilder({
     required _HashTagTextFieldState state,
-  })   : _state = state,
+  })  : _state = state,
         super(delegate: state);
 
   final _HashTagTextFieldState _state;
@@ -1419,7 +1419,7 @@ class _HashTagTextFieldState extends State<HashTagTextField>
 
     if (kIsWeb) {
       return Shortcuts(
-        shortcuts: scrollShortcutOverrides,
+        shortcuts: {},
         child: child,
       );
     }
